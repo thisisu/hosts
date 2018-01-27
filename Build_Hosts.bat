8496d9d5f0361c4fb99ca0eb2169fcbc960ccb71
thisisu <thisisu@github.com>
2018-01-26 22:41:44 -0600

    update

--- Build_Hosts.bat
+++ Build_Hosts.bat
@@ -9,7 +9,11 @@ COPY /Y "%hostsD%\hosts" "%TEMP%\hosts_fixme_000" >NUL 2>&1
 :: Start dumping
 GREP -P "^127\.94\.0\.(?:1|2|3)|^0\.0\.0\.0" <"%TEMP%\hosts_fixme_000" >"%TEMP%\hosts_fixme_001"
 GREP -vP "^0\.0\.0\.0 0\.0\.0\.0$" <"%TEMP%\hosts_fixme_001" >"%TEMP%\hosts_fixme_002"
+:: Now combine my hosts file
+TYPE "E:\Github\hosts\hosts" >>"%TEMP%\hosts_fixme_002"
+:: uniq and sort
 SORT_ -f -u <"%TEMP%\hosts_fixme_002" >"%TEMP%\hosts_fixme_003"
+:: create final host file
 TYPE "E:\Github\hosts\header">"%TEMP%\hosts_final"
 TYPE "%TEMP%\hosts_fixme_003">>"%TEMP%\hosts_final"
 COPY /Y "%TEMP%\hosts_final" "%hostsD%\hosts"
