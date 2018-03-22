8f06cff579fdbee69d5b2d9b292a04c7dcf3a63d
thisisu <thisisu@github.com>
2018-03-21 22:30:16 -0500

    update

--- sort.bat
+++ sort.bat
@@ -1,2 +1,4 @@
-SORT_ -f -u <"E:\Github\hosts\hosts" >"E:\Github\hosts\hosts_temp"
-MOVE /Y "E:\Github\hosts\hosts_temp" "E:\Github\hosts\hosts"
+SED -R "s/\:443$//;s/\/$//;s/^([a-z1-9-\.]+)/0\.0\.0\.0 \1/;/^\s*$/d" <"E:\Github\hosts\hosts" >"E:\Github\hosts\temp1"
+SORT_ -f -u <"E:\Github\hosts\temp1" >"E:\Github\hosts\temp2"
+MOVE /Y "E:\Github\hosts\temp2" "E:\Github\hosts\hosts"
+DEL /F/Q "E:\Github\hosts\temp*"
