3c0c479774f6018758b2bb847502cc2a179a7b49
thisisu <thisisu@github.com>
2018-07-27 18:21:23 -0500

    update mobile

--- sort.bat
+++ sort.bat
@@ -1,4 +1,4 @@
-SED -R "s/\:443$//;s/\/$//;s/^([a-z1-9-\.]+)/0\.0\.0\.0 \1/;/^\s*$/d" <"E:\Github\hosts\hosts" >"E:\Github\hosts\temp1"
+SED -R "s/\:443$//;s/\/$//;s/^([a-z1-9-\.]+)/0\.0\.0\.0 \1/;s/^(0\.[a-z]+\.com$)/0\.0\.0\.0 \1/;/^\s*$/d" <"E:\Github\hosts\hosts" >"E:\Github\hosts\temp1"
 SORT_ -f -u <"E:\Github\hosts\temp1" >"E:\Github\hosts\temp2"
 MOVE /Y "E:\Github\hosts\temp2" "E:\Github\hosts\hosts"
 DEL /F/Q "E:\Github\hosts\temp*"
