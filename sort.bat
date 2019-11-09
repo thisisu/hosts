d69484196755d26a4a152b33e6219a8d0c5a90fb
thisisu <thisisu@github.com>
2019-11-09 10:15:56 -0600

    update

--- sort.bat
+++ sort.bat
@@ -1,5 +1,6 @@
 SED -r "/^\s*[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\s+|^\s*#|.\s/!s/./0.0.0.0 &/;s/\:[0-9]{2,5}$//;/^\s*$/d" <"E:\Github\hosts\hosts" >"E:\Github\hosts\temp1"
 SORT_ -f -u <"E:\Github\hosts\temp1" >"E:\Github\hosts\temp2"
 MOVE /Y "E:\Github\hosts\temp2" "E:\Github\hosts\hosts"
+SORT_ -f -u <"E:\Github\hosts\IPs.txt" >"E:\Github\hosts\temp3ips"
+MOVE /Y "E:\Github\hosts\temp3ips" "E:\Github\hosts\IPs.txt"
 DEL /F/Q "E:\Github\hosts\temp*"
-Call "E:\Github\hosts\sort_ips.bat"
